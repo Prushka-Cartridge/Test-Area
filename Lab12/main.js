287856d2212d49718074d47196b6aeae709832ca
Prush <PrushkaCartridge@gmail.com>
2018-04-22 04:19:21 +0100

    G

--- Lab12/main.js
+++ Lab12/main.js
@@ -8,3 +8,33 @@ var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/r
 });
 
 Esri_WorldGrayCanvas.addTo(mymap);
+
+$('#shakey').click(function(){
+
+  console.log("getting quakes");
+
+  $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson",function(result){
+
+    console.log(result)
+
+    result.features.forEach(function(quake) {
+      var lng = quake.geometry.coordinates[0];
+      var lat = quake.geometry.coordinates[1];
+
+      var circle = L.circle([lat, lng], 1, {
+        color: 'red',
+        opacity: 0,
+        fillColor: 'red',
+        fillOpacity: 0.8
+      })
+
+      circle.addTo(mymap);
+
+    });
+
+
+
+
+  });
+
+});
